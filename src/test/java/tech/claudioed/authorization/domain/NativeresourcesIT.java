0c97fbcf287f85967102dc2b5680d77faefceee4
Claudio E. de Oliveira <claudioed.oliveira@gmail.com>
2019-04-07 19:49:00 -0300

    uncomment jaeger properties

--- src/test/java/tech/claudioed/authorization/domain/NativeresourcesIT.java
+++
@@ -1,9 +0,0 @@
-package tech.claudioed.authorization.domain;
-
-import io.quarkus.test.junit.SubstrateTest;
-
-@SubstrateTest
-public class NativeresourcesIT extends authorizationTest {
-
-    // Execute the same tests but in native mode.
-}
